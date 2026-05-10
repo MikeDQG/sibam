6c1d6148f4ccf445cab2f7bcbf3a1d8f8995528c
galbadrov <gal.badrov@gmail.com>
2026-05-11 00:35:00 +0200

    fix: popravljeno leto v footerju

--- frontend/src/components/LandingPageComponents/Footer.tsx
+++ frontend/src/components/LandingPageComponents/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <footer className='flex items-center justify-center bg-neutral-800 text-white py-8'>
-      © 2023 Šibam. All rights reserved.
+      © 2026 Šibam. All rights reserved.
     </footer>
   );
 };

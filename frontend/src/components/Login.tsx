d49157d5d3ce6dfb4dca1879ad37eb57a5927951
galbadrov <gal.badrov@gmail.com>
2026-05-10 15:02:55 +0200

    SCRUM-7 feat: dodan tailwind, odstranjen nepotreben default css in postavljena arhitektura za main tsx in login/register

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,7 @@
+export const Login = () => {
+  return (
+    <div className='flex items-center justify-center h-screen'>
+      <h1>login</h1>
+    </div>
+  );
+};
---
+++ frontend/src/components/Register.tsx
@@ -0,0 +1,7 @@
+export const Register = () => {
+  return (
+    <div className='flex items-center justify-center h-screen'>
+      <h1>register</h1>
+    </div>
+  );
+};

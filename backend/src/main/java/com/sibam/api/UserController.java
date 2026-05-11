61c173cc057b79d264619ccad1ad0aab406a76dd
Kaja <riposte_pins0t@icloud.com>
2026-05-11 17:27:02 +0200

    SCRUM-19 Implementirana registracija, prijava, Firebase avtentikacija, UserService, UserController in AccountPage

---
+++ backend/src/main/java/com/sibam/api/UserController.java
@@ -0,0 +1,33 @@
+package com.sibam.api;
+
+import com.sibam.model.User;
+import com.sibam.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/me")
+    public ResponseEntity<User> loginOrRegister(
+            @RequestAttribute("uid") String uid,
+            @RequestAttribute("email") String email) {
+        User user = userService.getOrCreateUser(uid, email);
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<User> getMe(
+            @RequestAttribute("uid") String uid) {
+        return userService.getUserByFirebaseUid(uid)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}

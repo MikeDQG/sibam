9e27533d482f2ccd94757255305c0afe108022e5
Kaja <riposte_pins0t@icloud.com>
2026-06-02 23:23:23 +0200

    fix: SchedulerService: injected Clock namesto neposredne uporabe OffsetDateTime.now(), da testi ne padejo zaradi ure zagona

--- backend/src/test/java/com/sibam/SibamApplicationTests.java
+++ backend/src/test/java/com/sibam/SibamApplicationTests.java
@@ -1,10 +1,31 @@
 package com.sibam;
 
+import com.sibam.integration.gtfsRT.GTFSRTClient;
+import com.sibam.integration.mbajk.MBajkClient;
+import com.sibam.integration.weather.WeatherClient;
+import com.sibam.service.BikePredictionService;
+import com.sibam.service.BusDelayPredictionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class SibamApplicationTests {
+
+    @MockitoBean
+    BikePredictionService bikePredictionService;
+
+    @MockitoBean
+    BusDelayPredictionService busDelayPredictionService;
+
+    @MockitoBean
+    MBajkClient mbajkClient;
+
+    @MockitoBean
+    GTFSRTClient gtfsRTClient;
+
+    @MockitoBean
+    WeatherClient weatherClient;
 
     @Test
     void contextLoads() {

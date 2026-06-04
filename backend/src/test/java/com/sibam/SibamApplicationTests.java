1d7a4c3d7ba22a37e5e7fd2c6991e13005c9c946
Kaja <riposte_pins0t@icloud.com>
2026-06-04 13:31:40 +0200

    SCRUM-140 Popravljen test za sibam application

--- backend/src/test/java/com/sibam/SibamApplicationTests.java
+++ backend/src/test/java/com/sibam/SibamApplicationTests.java
@@ -7,12 +7,35 @@ import com.sibam.service.BikePredictionService;
 import com.sibam.service.BusDelayPredictionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
-@SpringBootTest
-@ActiveProfiles("ci")
+@SpringBootTest(properties = {
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "supabase.url=http://dummy",
+        "supabase.service-key=dummy",
+        "mbajk.api.key=dummy",
+        "openweathermap.api.key=dummy",
+        "schedulers.fetch-bike-ingestion.on=false",
+        "schedulers.fetch-weather-ingestion.on=false",
+        "schedulers.fetch-bus-ingestion.on=false"
+})
 class SibamApplicationTests {
+
+    static final PostgreSQLContainer POSTGRES = new PostgreSQLContainer("postgres:16");
+
+    static {
+        POSTGRES.start();
+    }
+
+    @DynamicPropertySource
+    static void datasourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES::getUsername);
+        registry.add("spring.datasource.password", POSTGRES::getPassword);
+    }
 
     @MockitoBean
     BikePredictionService bikePredictionService;
@@ -32,5 +55,4 @@ class SibamApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
